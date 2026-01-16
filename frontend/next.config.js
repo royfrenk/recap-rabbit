0b79b5329e8ab85b199d6fd4c365e511ec7175b5
royfrenk <royfrenk@gmail.com>
2026-01-15 16:59:21 -0800

    Clean up unused env vars from next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    STAGING_AUTH: process.env.STAGING_AUTH,
-    STAGING_USER: process.env.STAGING_USER,
-    STAGING_PASS: process.env.STAGING_PASS,
-  },
   async rewrites() {
     const backendUrl = process.env.BACKEND_URL || 'http://localhost:8000'
     return [
